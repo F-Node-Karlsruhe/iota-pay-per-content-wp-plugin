4469e7e975df6e678ea1cbd245d129eeee26aa19
F-Node-Karlsruhe <undzn@student.kit.edu>
2021-10-31 13:59:52 +0100

    show price in widget

--- iotapaypercontent.php
+++ iotapaypercontent.php
@@ -3,7 +3,7 @@
  * Plugin Name: Iota Pay per Content
  * Plugin URL: https://pay-per-content.com
  * Description: Links your wordpress site to pay-per-content.com
- * Version: 0.1
+ * Version: 0.2
  * Author: pay-per-content.com
  * Author URI: https://pay-per-content.com
  * Tags: Pay per content, IOTA
@@ -24,14 +24,16 @@ function paid_shortcode( $atts, $content = null ) {
 
 	$slug = $post->post_name;
 
+	$price = json_decode(file_get_contents('https://pay-per-content.com/api/price/' . $domain . '/' . $slug));
+
 	ob_start();
     ?>
 	<div id="iota-pay-per-content-identifier-b88797071904e355cd79c9b0a965d" style="background: lightgray;
 				border-radius: 10px;
 				padding: 30px;">
 	<h5>This part is for paying readers only</h5>
-	<h5><a href="<?php echo esc_url( 'https://pay-per-content.com/gateway/get/' . $domain . '/' . $slug . '/' ); ?>">Buy the full article</a></h5>
-	<p style="font-size: 8px;">Powered by pay-per-content.com</p>
+	<h5><a href="<?php echo esc_url( 'https://pay-per-content.com/gateway/get/' . $domain . '/' . $slug . '/' ); ?>">Buy the full article</a><?php echo ' for ' . $price->{'price'} . ' ' . $price->{'currency'}?></h5>
+	<p style="font-size: 8px;">Powered by <a href="https://pay-per-content.com">pay-per-content.com</a></p>
 	</div>
 	<?php
     return ob_get_clean();
